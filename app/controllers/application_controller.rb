9f820557815bb01f361955a1f8ccd5cc7d791265
N A <up_sky_2@icloud.com>
2020-05-25 22:40:07 +0900

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
-
+  before_action :set_locale
   protect_from_forgery with: :exception
   include SessionsHelper
 
   private
 
-  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 
     # ユーザーのログインを確認する
     def logged_in_user
